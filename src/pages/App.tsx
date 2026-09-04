aaac053d09ba66967f090e422df0f88770862d99
fernandoandres.rodriguez <00174320@uca.edu.sv>
2026-09-04 16:49:49 -0600

    fix: remove fetch priority attribute from background image

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -11,7 +11,6 @@ function App() {
         src={background}
         draggable={false}
         alt="Background image"
-        fetchPriority="high"
       />
 
       <div className="relative z-10 pt-9 pb-28 w-full">
